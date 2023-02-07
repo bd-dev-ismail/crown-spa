d16bb70757f95d02f04508c2cda7241247d785b7
bd-dev-ismail <ismailratul7@gmail.com>
2023-02-07 17:00:56 +0600

    update footer

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import {Nav} from '../components/Nav/Nav';
-import {Footer} from '../components/Footer/Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
+import { Nav } from "../components/Nav/Nav";
+// import {Footer} from '../components/Footer/Footer';
 const Main = () => {
-    return (
-        <div>
-            <Nav/>
-            <Outlet/>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div>
+      <Nav />
+      <Outlet />
+      {/* <Footer/> */}
+      <Footer />
+    </div>
+  );
 };
 
 export default Main;
